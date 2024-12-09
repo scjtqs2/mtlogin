4e45846f57261c7036519a41bd0612bd86e76f73
davidelectricfree <davidelefree@outlook.com>
2024-12-09 18:09:11 +0800

    Update main.go

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func defaultCfg() *Config {
 		Ua:          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0",
 		CorpID:      "",
 		AgentSecret: "",
-		AgentID:     1000002,
+		AgentID:     0,
 	}
 }
 
